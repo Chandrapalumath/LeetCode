eab484caa67b9650f2173b9b8faa3b225ac34672
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2024-07-24 12:12:52 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (43.35%) - LeetHub

---
+++ 0677-map-sum-pairs/0677-map-sum-pairs.cpp
@@ -0,0 +1,26 @@
+class MapSum {
+public:
+    map<string,int>mp;
+    MapSum() {
+        
+    }
+    
+    void insert(string key, int val) {
+        mp[key]=val;
+    }
+    
+    int sum(string prefix) {
+        int ans=0;
+        for(auto a : mp){
+            if(a.first.find(prefix)==0) ans+=a.second;
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum* obj = new MapSum();
+ * obj->insert(key,val);
+ * int param_2 = obj->sum(prefix);
+ */
