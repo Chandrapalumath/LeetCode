720ac03630e4772ceb65aeb7c9fa1d24b233a379
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2024-08-01 11:21:39 +0530

    Time: 17 ms (91.69%), Space: 23 MB (39.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,7 +3,8 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>ans;
-        for(int i=0;i<intervals.size();i++){
+        //Brute Force Approach
+        /*for(int i=0;i<intervals.size();i++){
             int b=intervals[i][1],j=i+1,in=0;
             int max=b;
             while(j<intervals.size()){
@@ -16,6 +17,12 @@ public:
             }
             ans.push_back({intervals[i][0],max});
             i=j-1;
+        }*/
+        //Optimal Approach
+        ans.push_back(intervals[0]);
+        for(int i=0;i<intervals.size();i++){
+            if(intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
+            else ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
         return ans;
     }
