a49faf52604adb5c887ac01d02a2e114065c6c8b
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2024-08-30 12:36:38 +0530

    Time: 2 ms (37.07%), Space: 8 MB (30.81%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int c=0;
-        while(n){
-            int p=n%2;
-            if(p==1) c++;
-            n/=2;
-        }
-        return c;
+        return __popcount(n);
     }
 };
