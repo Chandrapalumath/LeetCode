6dea279a9361ffd9c932637c49412880ba250d8d
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2024-10-19 18:14:32 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (47.18%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int findComplement(int num) {
-        for(long i=1;i<=num;i<<=1) num ^= i;
+        if(num == 0){
+            return 1;
+        }
+
+        for(long i = 1; i <= num; i *= 2){
+            num ^= i;
+        }
         return num;
     }
 };
