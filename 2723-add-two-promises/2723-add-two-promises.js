52f96e98a7a92cd7d19c579f9192eec3f0527bc4
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2024-10-25 06:57:25 +0530

    Time: 64 ms (50.48%), Space: 49.2 MB (83.52%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -6,7 +6,7 @@
 var addTwoPromises = async function(promise1, promise2) {
     const ans=await Promise.all([promise1,promise2]);
     return ans[0]+ans[1];
-};
+}
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
