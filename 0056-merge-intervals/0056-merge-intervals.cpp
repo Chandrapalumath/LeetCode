a2344496b39f1aca56ff3c2c99820e31291c59b6
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2025-06-23 15:57:15 +0530

    Time: 8 ms (28%), Space: 23.6 MB (93.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -20,7 +20,7 @@ public:
         }*/
         //Optimal Approach
         ans.push_back(intervals[0]);
-        for(int i=0;i<intervals.size();i++){
+        for(int i=1;i<intervals.size();i++){
             if(intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
             else ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
