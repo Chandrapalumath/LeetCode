3b27729ebb383c3bbabcc3ceb74c9165b68a3138
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2025-07-27 23:54:18 +0530

    Time: 28 ms (41.4%), Space: 27.2 MB (77.09%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,18 +3,26 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return recur(text1,text2,n-1,m-1,dp);
-    }
-    int recur(string& text1, string& text2,int i, int j, vector<vector<int>>& dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]){
-            return dp[i][j]=1+recur(text1,text2,i-1,j-1,dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        //Base Case Handling
+        for (int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+            }
         }
-            int pick1 = 0+recur(text1,text2,i-1,j,dp);
-            int pick2 = 0+recur(text1,text2,i,j-1,dp);
-            dp[i][j]=max(pick1,pick2);
-        return dp[i][j];
+        for (int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]= 1+dp[i-1][j-1];//recur(text1,text2,i-1,j-1,dp);
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+                
+            }
+        }
+        return dp[n][m];
     }
+   
 };
+
+//Recursion -> Memoization -> Tabulation ->
