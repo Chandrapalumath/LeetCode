dcd6e612042161ffc216acc41623bc44d6518aa2
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2025-07-29 10:47:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size()-1;
-        vector<int>dp(n+1,-1);
-        return recur(nums,n,dp);
-    }
-    int recur(vector<int>& nums, int n, vector<int>& dp){
-        if(n<0){
-            return 0;
+        int n=nums.size();
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;i++){
+            int pick = nums[i-1]+ dp[i-2]; //recur(nums,n-2,dp);
+            int nopick = dp[i-1]; //recur(nums,n-1,dp);
+            dp[i] = max(pick,nopick);
         }
-        if(dp[n]!=-1) return dp[n];
-        int pick = nums[n]+recur(nums,n-2,dp);
-        int nopick = recur(nums,n-1,dp);
-        dp[n] = max(pick,nopick);
-        return max(pick,nopick);
+        return dp[n];
     }
 };
 
@@ -37,4 +34,28 @@ public:
 */
 
 // DP -> Memoization
-/**/
+/*
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+        vector<int>dp(n+1,-1);
+        return recur(nums,n,dp);
+    }
+    int recur(vector<int>& nums, int n, vector<int>& dp){
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1) return dp[n];
+        int pick = nums[n]+recur(nums,n-2,dp);
+        int nopick = recur(nums,n-1,dp);
+        dp[n] = max(pick,nopick);
+        return max(pick,nopick);
+    }
+};
+*/
+
+// DP -> Tabulation
+/*
+
+*/
