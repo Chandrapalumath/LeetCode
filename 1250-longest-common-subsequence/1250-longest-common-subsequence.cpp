02954cd1f8eedeeafc27e43b687b04138008178f
Chandrapalumath <133040993+Chandrapalumath@users.noreply.github.com>
2025-08-05 11:19:15 +0530

    Time: 23 ms (79.99%), Space: 27.4 MB (51.29%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -3,25 +3,21 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return recur(text1, text2, n - 1, m - 1, dp);
-    }
-
-    int recur(string &text1, string &text2, int n, int m, vector<vector<int>> &dp) {
-        if (n < 0 || m < 0) return 0;
-        if (dp[n][m] != -1) return dp[n][m];
-
-        if (text1[n] == text2[m]) {
-            return dp[n][m] = 1 + recur(text1, text2, n - 1, m - 1, dp);
-        } else {
-            return dp[n][m] = max(
-                recur(text1, text2, n - 1, m, dp),
-                recur(text1, text2, n, m - 1, dp)
-            );
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
+        dp[0][0]=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];//recur(text1, text2, n - 1, m - 1, dp);
+                else{
+                    dp[i][j] = max( dp[i-1][j] , dp[i][j-1] );
+                }
+            }
         }
-    }
-};
 
+        return dp[n][m];
+    }
+
+};
 
 // Recursive code for lcs
 /*
@@ -45,5 +41,39 @@ public:
         return max(pick,nopick);
     }
 };
+
+*/
+
+// Memoization code for lcs
+/*
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return recur(text1, text2, n - 1, m - 1, dp);
+    }
+
+    int recur(string &text1, string &text2, int n, int m, vector<vector<int>> &dp) {
+        if (n < 0 || m < 0) return 0;
+        if (dp[n][m] != -1) return dp[n][m];
+
+        if (text1[n] == text2[m]) {
+            return dp[n][m] = 1 + recur(text1, text2, n - 1, m - 1, dp);
+        } else {
+            return dp[n][m] = max(
+                recur(text1, text2, n - 1, m, dp),
+                recur(text1, text2, n, m - 1, dp)
+            );
+        }
+    }
+};
+*/
+
+// Tabulation code for lcs
+/*
+
+
 
 */
